b147be80a25550dd5a3eb57de7e0301ab3c173cb
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 01:15:49 +0100

    Apply swift-format and lint fixes

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-iso-3166",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11)
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26)
     ],
     products: [
         .library(name: .iso3166, targets: [.iso3166]),
